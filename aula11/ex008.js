85c34203a44650c638fb7780f52a93c377cefe1d
camunari <carla.munari95@gmail.com>
2022-12-21 10:44:28 -0300

    atualizações do projeto

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+console.log('o console funcionou corretamente')
+
+var vel = 79
+console.log(`A velocidade do seu carro é: ${vel}Km/h`)
+if (vel > 60) { //condicao simples
+    console.log(`Você excedeu a velocidade limite! Multado.`)    
+}
